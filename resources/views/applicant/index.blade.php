bdc75984c3f489de269b76af03385163548aee46
Jiyui <37652463+Jiyui@users.noreply.github.com>
2022-05-07 00:56:04 +0800

    Update index.blade.php

--- resources/views/applicant/index.blade.php
+++ resources/views/applicant/index.blade.php
@@ -8,7 +8,7 @@
                 <h2>Applicants</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-success" href="{{ route('Applicant.create') }}" title="Create a applicant"> <i class="fas fa-plus-circle"></i>
+                <a class="btn btn-success" href="{{ route('applicants.create') }}" title="Create a applicant"> <i class="fas fa-plus-circle"></i>
                 </a>
             </div>
         </div>
@@ -19,7 +19,6 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-
     <table class="table table-bordered table-responsive-lg">
         <tr>
             <th>ID</th>
@@ -32,29 +31,28 @@
             <th>Account Status</th>
             <th width="200px">Action</th>
         </tr>
-        @foreach ($applicant as $applicant)
+        @foreach ($applicants as $applicant)
             <tr>
                 <td>{{ $applicant->id }}</td>
                 <td>{{ $applicant->Region }}</td>
                 <td>{{ $applicant->Province }}</td>
                 <td>{{ $applicant->City }}</td>
-                <td>{{ $applicant->Firstname }}</td>
-                <td>{{ $applicant->Middlename }}</td>
-                <td>{{ $applicant->Lastname }}</td>
+                <td>{{ $applicant->firstname }}</td>
+                <td>{{ $applicant->middlename }}</td>
+                <td>{{ $applicant->lastname }}</td>
                 <td>{{ $applicant->accountstatus }}</td>
                 <td>
-                    <form action="{{ route('Applicant.destroy', $applicant->id) }}" method="POST">
+                    <form action="{{ route('applicants.destroy', $applicant->id) }}" method="POST">
 
-                        <a href="{{ route('Applicant.show', $applicant->id) }}" title="show">
+                        <a href="{{ route('applicants.show', $applicant->id) }}" title="show">
                             <i class="fas fa-eye text-success  fa-lg"></i>
                         </a>
 
-                        <a href="{{ route('Applicant.edit', $applicant->id) }}">
+                        <a href="{{ route('applicants.edit',  $applicant->id) }}">
                             Edit
                         </a>
                         @csrf
                         @method('DELETE')
-
                         <button type="submit" title="delete" style="border: none; background-color:transparent;">
                             delete
                         </button>
