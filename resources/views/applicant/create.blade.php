735dd8eb33600f7025baecfd44bf97dce4379efa
Jiyui <37652463+Jiyui@users.noreply.github.com>
2022-05-07 17:40:01 +0800

    Update create.blade.php

--- resources/views/applicant/create.blade.php
+++ resources/views/applicant/create.blade.php
@@ -1,115 +1,118 @@
-@extends('layout.app')
+@extends('home')
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Add New Priority</h2>
+    <div class="card container-sm">
+        <div class="card-header">
+            <div class="col-lg-12 margin-tb">
+                <div class="pull-left">
+                    <h2>Add New Applicant</h2>
+                </div>
+                <div class="pull-right">
+                    <a class="btn btn-primary" href="{{ route('applicants.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
+                </div>
             </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('applicants.index') }}" title="Go back"> <i class="fas fa-backward "></i> </a>
-            </div>
+        </div>
+        <div class="card-body">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form action="{{ route('applicants.store') }}" method="POST" >
+                @csrf
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>ID:</strong>
+                            <input type="text" name="id" class="form-control" placeholder="id">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Region:</strong>
+                            <input type="text" name="Region" class="form-control" placeholder="Region">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Province:</strong>
+                            <input type="text" name="Province" class="form-control" placeholder="Province">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>City:</strong>
+                            <input type="text" name="City" class="form-control" placeholder="City">
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Firstname:</strong>
+                            <textarea class="form-control" style="height:50px" name="Firstname"
+                                    placeholder="Firstname"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Middlename:</strong>
+                            <textarea class="form-control" style="height:50px" name="Middlename"
+                                    placeholder="Middlename"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Lastname:</strong>
+                            <textarea class="form-control" style="height:50px" name="Lastname"
+                                    placeholder="Lastname"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Sex:</strong>
+                            <textarea class="form-control" style="height:50px" name="Sex"
+                                    placeholder="Sex"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Age:</strong>
+                            <textarea class="form-control" style="height:50px" name="Age"
+                                    placeholder="Age"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Maritalstatus:</strong>
+                            <textarea class="form-control" style="height:50px" name="Maritalstatus"
+                                    placeholder="Maritalstatus"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>course:</strong>
+                            <textarea class="form-control" style="height:50px" name="course"
+                                    placeholder="course"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>positionapplied:</strong>
+                            <textarea class="form-control" style="height:50px" name="positionapplied"
+                                    placeholder="positionapplied"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+
+            </form>
         </div>
     </div>
-
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form action="{{ route('applicants.store') }}" method="POST" >
-        @csrf
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>ID:</strong>
-                    <input type="text" name="id" class="form-control" placeholder="id">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Region:</strong>
-                    <input type="text" name="Region" class="form-control" placeholder="Region">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Province:</strong>
-                    <input type="text" name="Province" class="form-control" placeholder="Province">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>City:</strong>
-                    <input type="text" name="City" class="form-control" placeholder="City">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Firstname:</strong>
-                    <textarea class="form-control" style="height:50px" name="Firstname"
-                              placeholder="Firstname"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Middlename:</strong>
-                    <textarea class="form-control" style="height:50px" name="Middlename"
-                              placeholder="Middlename"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Lastname:</strong>
-                    <textarea class="form-control" style="height:50px" name="Lastname"
-                              placeholder="Lastname"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Sex:</strong>
-                    <textarea class="form-control" style="height:50px" name="Sex"
-                              placeholder="Sex"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Age:</strong>
-                    <textarea class="form-control" style="height:50px" name="Age"
-                              placeholder="Age"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Maritalstatus:</strong>
-                    <textarea class="form-control" style="height:50px" name="Maritalstatus"
-                              placeholder="Maritalstatus"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>course:</strong>
-                    <textarea class="form-control" style="height:50px" name="course"
-                              placeholder="course"></textarea>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>positionapplied:</strong>
-                    <textarea class="form-control" style="height:50px" name="positionapplied"
-                              placeholder="positionapplied"></textarea>
-                </div>
-            </div>
-
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </div>
-
-    </form>
 @endsection
