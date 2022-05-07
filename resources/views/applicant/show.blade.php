05bdf53dab5c0ab0a184d5a1a570e7b6f8f4f9d6
Jiyui <37652463+Jiyui@users.noreply.github.com>
2022-05-07 17:29:52 +0800

    Update show.blade.php

--- resources/views/applicant/show.blade.php
+++ resources/views/applicant/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.app')
+@extends('home')
 
 
 @section('content')
@@ -12,81 +12,80 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="row">
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Region:</strong>
-                        {{ $applicant->Region }}
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Region:</strong>
+                            {{ $applicant->Region }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Province:</strong>
+                            {{ $applicant->Province }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>City:</strong>
+                            {{ $applicant->City }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Firstname:</strong>
+                            {{ $applicant->firstname }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Middlename:</strong>
+                            {{ $applicant->middlename }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Lastname:</strong>
+                            {{ $applicant->lastname }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Account Status:</strong>
+                            {{ $applicant->accountstatus }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Sex:</strong>
+                            {{ $applicant->sex }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Age:</strong>
+                            {{ $applicant->age }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Marital Status:</strong>
+                            {{ $applicant->maritalstatus }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Course:</strong>
+                            {{ $applicant->course }}
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Position Applied:</strong>
+                            {{ $applicant->positionapplied }}
+                        </div>
                     </div>
                 </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Province:</strong>
-                        {{ $applicant->Province }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>City:</strong>
-                        {{ $applicant->City }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Firstname:</strong>
-                        {{ $applicant->firstname }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Middlename:</strong>
-                        {{ $applicant->middlename }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Lastname:</strong>
-                        {{ $applicant->lastname }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Account Status:</strong>
-                        {{ $applicant->accountstatus }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Sex:</strong>
-                        {{ $applicant->sex }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Age:</strong>
-                        {{ $applicant->age }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Marital Status:</strong>
-                        {{ $applicant->maritalstatus }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Course:</strong>
-                        {{ $applicant->course }}
-                    </div>
-                </div>
-                <div class="col-xs-12 col-sm-12 col-md-12">
-                    <div class="form-group">
-                        <strong>Position Applied:</strong>
-                        {{ $applicant->positionapplied }}
-                    </div>
-                </div>
-
-            </div>
         </div>
     </div>
 @endsection
