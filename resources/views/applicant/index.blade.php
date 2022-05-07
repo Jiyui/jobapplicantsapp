9af1c021560578b1f1774d58ff32341422632c51
Jiyui <37652463+Jiyui@users.noreply.github.com>
2022-05-07 17:40:06 +0800

    Update index.blade.php

--- resources/views/applicant/index.blade.php
+++ resources/views/applicant/index.blade.php
@@ -21,47 +21,47 @@
             </div>
         @endif
         <table class="table table-bordered table-responsive-lg">
-        <tr>
-            <th>ID</th>
-            <th>Region</th>
-            <th>Province</th>
-            <th>city</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Last Name</th>
-            <th>Account Status</th>
-            <th width="200px">Action</th>
-        </tr>
-        @foreach ($applicants as $applicant)
             <tr>
-                <td>{{ $applicant->id }}</td>
-                <td>{{ $applicant->Region }}</td>
-                <td>{{ $applicant->Province }}</td>
-                <td>{{ $applicant->City }}</td>
-                <td>{{ $applicant->firstname }}</td>
-                <td>{{ $applicant->middlename }}</td>
-                <td>{{ $applicant->lastname }}</td>
-                <td>{{ $applicant->accountstatus }}</td>
-                <td>
-                    <form action="{{ route('applicants.destroy', $applicant->id) }}" method="POST">
-
-                        <a href="{{ route('applicants.show', $applicant->id) }}" title="show">
-                            <i class="fas fa-eye text-success  fa-lg"></i>
-                        </a>
-
-                        <a href="{{ route('applicants.edit',  $applicant->id) }}">
-                            Edit
-                        </a>
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                            delete
-                        </button>
-                    </form>
-                </td>
+                <th>ID</th>
+                <th>Region</th>
+                <th>Province</th>
+                <th>city</th>
+                <th>First Name</th>
+                <th>Middle Name</th>
+                <th>Last Name</th>
+                <th>Account Status</th>
+                <th width="200px">Action</th>
             </tr>
-        @endforeach
-    </table>
+            @foreach ($applicants as $applicant)
+                <tr>
+                    <td>{{ $applicant->id }}</td>
+                    <td>{{ $applicant->Region }}</td>
+                    <td>{{ $applicant->Province }}</td>
+                    <td>{{ $applicant->City }}</td>
+                    <td>{{ $applicant->firstname }}</td>
+                    <td>{{ $applicant->middlename }}</td>
+                    <td>{{ $applicant->lastname }}</td>
+                    <td>{{ $applicant->accountstatus }}</td>
+                    <td>
+                        <form action="{{ route('applicants.destroy', $applicant->id) }}" method="POST">
+
+                            <a href="{{ route('applicants.show', $applicant->id) }}" title="show">
+                                <i class="fas fa-eye text-success  fa-lg"></i>
+                            </a>
+
+                            <a href="{{ route('applicants.edit',  $applicant->id) }}">
+                                Edit
+                            </a>
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                                delete
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 </div>
 @endsection
